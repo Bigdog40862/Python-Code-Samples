74a455b2927d7560223c22861adeb645d6b06528
Emmett C <ecotton1@my.nl.edu>
2020-02-12 11:14:34 -0500

    Add files via upload

---
+++ Mod 3 Prob. 4.py
@@ -0,0 +1,62 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 22:39:24) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:/Users/User/Documents/Hello World.py ===============
+Hello World!
+>>> 
+============== RESTART: C:/Users/User/Documents/Mod 3 Problem 2.py =============
+What is your name?Moshe
+Moshe
+>>> 
+============== RESTART: C:/Users/User/Documents/Mod 3 Problem 2.py =============
+What is your name?Moshe
+Welcome Moshe
+>>> print("moshe")
+moshe
+>>> 12*200
+2400
+>>> 
+============== RESTART: C:/Users/User/Documents/Mod 3 Problem 2.py =============
+What is your name?Emmett
+Welcome Emmett
+>>> 
+================== RESTART: C:/Users/User/Documents/prob. 4.py =================
+What is the radius of your circle?5
+>>> radius
+'5'
+>>> radius+2
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    radius+2
+TypeError: can only concatenate str (not "int") to str
+>>> radius=int(radius)
+>>> radius
+5
+>>> radius+2
+7
+>>> radius
+5
+>>> 5^2
+7
+>>> 5**2
+25
+>>> 
+================== RESTART: C:/Users/User/Documents/prob. 4.py =================
+What is the radius of your circle?4
+Traceback (most recent call last):
+  File "C:/Users/User/Documents/prob. 4.py", line 10, in <module>
+    area = pi*radius**2
+NameError: name 'pi' is not defined
+>>> 
+================== RESTART: C:/Users/User/Documents/prob. 4.py =================
+What is the radius of your circle?5
+78.5
+>>> 
+================== RESTART: C:/Users/User/Documents/prob. 4.py =================
+What is the radius of your circle?25
+The answer is 1962.5
+>>> 
+=============== RESTART: C:/Users/User/Documents/Mod 3 Prob.1.py ===============
+What is your name?Emmett
+Welcome Emmett
+>>> 
