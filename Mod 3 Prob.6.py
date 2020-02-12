26e5157273885184e29d3850745a164f2a5c7cc3
Emmett C <ecotton1@my.nl.edu>
2020-02-12 11:16:02 -0500

    Add files via upload

---
+++ Mod 3 Prob.6.py
@@ -0,0 +1,6 @@
+#Emmett Cotton
+#02/10/2020
+#Lab Activity Prob. 6
+c=int(input("please enter temperture in degrees:"))
+f=c * 9/5 +32
+print("the temperture in degrees celeius was {0}, this is{1} in degreesfarenheit".format(c,f))
