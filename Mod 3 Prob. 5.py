32c63aecf3b3f764fa58a6f8d71c7a4723f75a52
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-02-20 16:42:08 -0600

    Update Mod 3 Prob. 5.py

--- Mod 3 Prob. 5.py
+++ Mod 3 Prob. 5.py
@@ -3,4 +3,6 @@
 #Mod. 3 Lab Activity 5
 miles=input("miles driven:")
 gas=input("gallons used:")
+#insert a line similar to this:  MPG = Miles / gas
 print("Thank you for your input")
+#modify the line above to print MPG too --> print("Thank you for your input", MPG)
