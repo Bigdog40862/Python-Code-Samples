9549b348086436ae3a06bafcdcd174df9a5ef6f1
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-02-20 16:23:15 -0600

    Update Mod. 3 Lab Activity Prob. 1.py

--- Mod. 3 Lab Activity Prob. 1.py
+++ Mod. 3 Lab Activity Prob. 1.py
@@ -2,3 +2,4 @@ print("Hello World!")
 #Emmett Cotton
 #02/10/2020
 #This program is for Mod. 3 Lab Activity Prob.1
+#Generally the documentation about the program author and content comes at the start of the program.  Please follow this convention.
