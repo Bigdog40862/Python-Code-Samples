d13ee31cc078e6557968ef15245034ffe2706306
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-02-20 16:33:45 -0600

    Update Mod 3 Prob.3.py

--- Mod 3 Prob.3.py
+++ Mod 3 Prob.3.py
@@ -4,3 +4,14 @@
 name=input("What is your name?")
 name= ("Emmett, or Laleh", only)
 print("Hello", name)
+# your program doesn't run.
+#Use If statement. See example below
+#name = str(input("Please enter your name:"))
+#if name == "Student": # Add your name
+#	print("Hello,",name,"!")
+	
+#elif name == "Laleh": #Add instuctor's name
+#                print("Hello,",name,"!")
+	
+#else:
+#	print("Name not recognized.")
